d56f96dbabb45ece41d5992304bf5b02a624be81
Nicolas Pelletier <nicolas.pelletier@agr.gc.ca>
2023-04-27 14:41:32 -0400

    Fixed discontinuities do to bad branch merging

--- scripts/Recent/Top30cm_SOC_estimator.R
+++ scripts/Recent/Top30cm_SOC_estimator.R
@@ -9,7 +9,7 @@ get.stocks <- function(site_data) {
       site = UniqueStockID,
       year = year_name,
       depthID = paste0(soil_depth_min_cm,"-",soil_depth_max_cm)) %>%
-    select(site,year,Exp_ID,soc_tha,soil_depth_min_cm,soil_depth_max_cm,depthID) %>%
+    select(site,year,Exp_ID,TrtID,soc_tha,soil_depth_min_cm,soil_depth_max_cm,depthID) %>%
     distinct() %>%
     arrange(site,year) %>%
     mutate(soc_tha=as.numeric(soc_tha)) 
@@ -72,12 +72,15 @@ return(correct.fac)
 
 # Function to estimate 30 cm SOC at all sites and all dates
 top.SOC.estimate <- function(df,correct.fac,depth_cm){
+  df <- df[!is.na(df$soc_tha),]
+  df <- df[df$soc_tha!=0,]
   
   for (site in unique(df$site)){
     sub.df=df[df$site==site,]
     for (year in unique(sub.df$year)){
       sub.df2=sub.df[sub.df$year==year,] 
-      type_string=paste(list(unique(sub.df2$depthID)))
+      type_string=unique(sub.df2$depthID) %>% sort %>% paste(collapse = " + ")
+      TrtID=unique(sub.df2$TrtID)
       
       res.df=data.frame(depth.min=seq(0,depth_cm-0.5,0.5),
                         depth.max=seq(0.5,depth_cm,0.5),
@@ -98,7 +101,7 @@ top.SOC.estimate <- function(df,correct.fac,depth_cm){
         data=sub.df2[row,]
         res.df[,row+3]=ifelse(res.df$depth.min>=data$soil_depth_min_cm & res.df$depth.max<=data$soil_depth_max_cm,
                               data$soc_tha/((data$soil_depth_max_cm-data$soil_depth_min_cm)*2),
-                              res.df$measured_c)
+                              NA)
       
     res.df <- replace(res.df, res.df==0, NA)
     
@@ -114,6 +117,7 @@ top.SOC.estimate <- function(df,correct.fac,depth_cm){
       }}
     out=data.frame(site,
                    experiment=sub("\\_.*", "", site),
+                   TrtID,
                    year,
                    input_type=type_string,
                    modelled_SOC)
@@ -126,3 +130,50 @@ top.SOC.estimate <- function(df,correct.fac,depth_cm){
 }
 
 
+top.SOC.raw <- function(df){
+  df <- df[!is.na(df$soc_tha),]
+  df <- df[df$soc_tha!=0,]
+  
+  for (site in unique(df$site)){
+    sub.df=df[df$site==site,]
+    for (year in unique(sub.df$year)){
+      sub.df2=sub.df[sub.df$year==year,] 
+      type_string=unique(sub.df2$depthID) %>% sort %>% paste(collapse = " + ")
+      TrtID=unique(sub.df2$TrtID)
+      if (length(TrtID)>1){print(paste0("error: more than one treatment ID for stockID --> ",site))}
+      d.max=max(sub.df2$soil_depth_max_cm)
+      res.df=data.frame(depth.min=seq(0,d.max-0.5,0.5),
+                        depth.max=seq(0.5,d.max,0.5),
+                        measured_c=NA)
+      
+      for (row in 1:nrow(sub.df2)){
+        data=sub.df2[row,]
+        res.df[,row+3]=ifelse(res.df$depth.min>=data$soil_depth_min_cm & res.df$depth.max<=data$soil_depth_max_cm,
+                              data$soc_tha/((data$soil_depth_max_cm-data$soil_depth_min_cm)*2),
+                              NA)
+        
+        res.df <- replace(res.df, res.df==0, NA)
+        
+        res.df$measured_c=rowMeans(data.frame(res.df[,4:length(res.df)]),na.rm=T)
+        
+        # Filling missing values in top / bottom section(s)
+        # Can we use the DSM estimation instead?
+        #res.df <- res.df %>%
+        #fill(measured_c, .direction = "down")%>%
+        #fill(measured_c, .direction = "up")
+        
+        modelled_SOC=sum(res.df$measured_c)
+      }
+      out=data.frame(site,
+                     experiment=sub("\\_.*", "", site),
+                     TrtID,
+                     year,
+                     input_type=type_string,
+                     modelled_SOC)
+      
+      if (site==unique(stocks.df$site)[1] & year==unique(sub.df$year)[1]){final=out } else {final=rbind(final,out)}
+      
+    }
+  }
+  return(final)
+}
